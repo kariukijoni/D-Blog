fd567b1236d371e8b2334b6d6efc02d3e2364f34
kariukijoni <kariukijoni@gmail.com>
2023-09-15 13:34:39 +0300

    [Added] filters by author

--- blog/views.py
+++ blog/views.py
@@ -35,6 +35,17 @@ class PostListView(ListView):
     paginate_by = 2
 
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
 class PostDetailView(DetailView):
     model = Post
 
